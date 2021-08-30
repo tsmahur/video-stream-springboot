56052c3f2a5b7f0ce3cb51a1bb4f731fda1af5ec
tsmahur <32067811+tsmahur@users.noreply.github.com>
2021-08-30 16:14:55 +0530

    added spring web dependency and config file both for CORS

---
+++ src/main/java/com/tsm/stream/config/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.tsm.stream.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedMethods("GET","PUT","POST","DELETE","PATCH")
+//                        .allowedOrigins("http://localhost:8080")
+                ;
+            }
+        };
+    }
+}
+
+
